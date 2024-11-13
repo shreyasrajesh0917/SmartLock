4c5e85338b01e1f6895450a7a8dd672c9b756269
Shreyas Rajesh <shreyasrajesh0917@gmail.com>
2024-11-13 11:31:09 -0800

    Lock code done

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,68 @@
+// #include <Arduino.h>
+// #include <Keypad.h>
+// #include <string>
+
+// using namespace std;
+
+// char matrix[4][4] = {{'1', '2', '3', 'A'}, {'4', '5', '6', 'B'}, {'7', '8', '9', 'C'}, {'*', '0', '#', 'D'}};
+// byte rowPins[4] = {26, 25, 33, 32};
+// byte colPins[4] = {13, 12, 14, 27};
+
+// Keypad keypad = Keypad(makeKeymap(matrix), rowPins, colPins, 4, 4);
+
+// string passkey = "AB12";
+// string current = "";
+
+// void setup()
+// {
+//   // Initialize serial communication
+//   Serial.begin(9600);
+// }
+
+// void loop()
+// {
+//   char key = keypad.getKey();
+
+//   if (key)
+//   {
+//     //Serial.println(key);
+//     if (current.size() > passkey.size())
+//     {
+//       Serial.println("Passkey too long");
+//       // Make LED turn red
+//       delay(2000);
+//       current = "";
+//     }
+
+//     if (key != '*' && current.size() < passkey.size())
+//     {
+//       Serial.println(key); // optional for testing
+//       current += key;
+//     }
+//     else if (key == '*' && !current.empty())
+//     {
+//       if (current == passkey)
+//       {
+//         Serial.println("Correct passkey");
+//         // Make LED turn green
+//         delay(2000);
+//         // Send signal to unlock
+//         current = "";
+//         // Wait a little bit, then lock it back
+//       }
+//       else
+//       {
+//         Serial.println("Wrong passkey");
+//         // Make LED turn red
+//         delay(2000);
+//         current = "";
+//       }
+//     }
+//   }
+// }
+
 #include <Arduino.h>
 #include <Keypad.h>
-#include <string>
-
-using namespace std;
 
 char matrix[4][4] = {{'1', '2', '3', 'A'}, {'4', '5', '6', 'B'}, {'7', '8', '9', 'C'}, {'*', '0', '#', 'D'}};
 byte rowPins[4] = {26, 25, 33, 32};
@@ -10,12 +70,12 @@ byte colPins[4] = {13, 12, 14, 27};
 
 Keypad keypad = Keypad(makeKeymap(matrix), rowPins, colPins, 4, 4);
 
-string passkey = "AB12";
-string current = "";
+char passkey[] = "AB12\0";
+char current[5] = ""; // 4 characters + null terminator
+int currentIndex = 0; // Renamed from 'index' to 'currentIndex'
 
 void setup()
 {
-  // Initialize serial communication
   Serial.begin(9600);
 }
 
@@ -25,36 +85,37 @@ void loop()
 
   if (key)
   {
-    if (current.size() > passkey.size())
-    {
-      Serial.println("Passkey too long");
-      // Make LED turn red
-      delay(2000);
-      current = "";
-    }
+    // Serial.println(key); // For testing keypress
 
-    if (key != '*' && current.size() < passkey.size())
+    if (key != '*')
     {
-      Serial.println(key); // optional for testing
-      current += key;
+      if (currentIndex < sizeof(passkey) - 2)
+      {
+        current[currentIndex] = key;
+        currentIndex++;
+        Serial.println(current);
+      }
+      else
+      {
+        Serial.println("Passkey too long");
+        memset(current, '\0', sizeof(current)); // Reset the entire array
+        currentIndex = 0;
+      }
     }
-    else if (key == '*' && !current.empty())
+    else
     {
-      if (current == passkey)
+      current[currentIndex] = '\0'; // Null-terminate the string
+      if (strcmp(current, passkey) == 0)
       {
         Serial.println("Correct passkey");
-        // Make LED turn green
-        delay(2000);
-        // Send signal to unlock
-        current = "";
+        // Trigger your unlock logic here
       }
       else
       {
         Serial.println("Wrong passkey");
-        // Make LED turn red
-        delay(2000);
-        current = "";
       }
+      currentIndex = 0;                       // Reset for next attempt
+      memset(current, '\0', sizeof(current)); // Reset the entire array
     }
   }
 }
